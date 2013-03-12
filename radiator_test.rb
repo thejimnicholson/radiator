8ddfb7e09010313df5e2b5e090fa476323337c66
Jim Nicholson <thejimnicholson@gmail.com>
2013-03-12 01:36:10 -0400

    Improved tests

--- radiator_test.rb
+++ radiator_test.rb
@@ -10,22 +10,33 @@ class RadiatorTest <  Test::Unit::TestCase
   end
 
   def setup
+    flexmock(Resolv).new_instances.should_receive(:getname => 'localhost')
     @client = client_for_tests
   end
 
   def teardown
-    Client.all.each {|c| c.destroy }
+    Client.all.each {|c| c.destroy! }
+    View.all.each {|v| v.destroy! }
+    Source.all.each {|s| s.destroy! }
   end
 
-  def test_naked_get
-    get '/'
+  def test_get_existing_client
+    get '/',{}
     assert last_response.ok?
+  end
+
+  def test_get_unknown_ip
+    get '/', {}, { 'REMOTE_ADDR' => '127.0.0.2' }
+    assert last_response.ok?
+    assert !Client.get('127.0.0.2').nil?, 'Should have created a record'
   end
 
   def test_client_find_or_create_by_ip_for_new
     client = Client.find_or_create_by_ip('127.0.0.2')
+    client.save!
     assert !client.nil?, 'should return a new client'
     assert_equal('127.0.0.2', client.ip.to_s, 'should set ip address')
+    assert !Client.get('127.0.0.2').nil?, 'Should have created a record'
   end
 
   def test_client_find_or_create_by_ip_for_existing
@@ -34,43 +45,41 @@ class RadiatorTest <  Test::Unit::TestCase
   end
 
   def test_client_lookup_host_no_value
-    flexmock(Resolv).new_instances.should_receive(:getname => 'localhost')
     assert_equal('localhost',@client.lookup_host(nil),'should be localhost')
   end
 
-  def test_client_lookup_host_has_value
+  def test_client_lookup_host_has_forced_value
     @client.host = nil
     @client.save
     assert_equal('somevalue',@client.lookup_host('somevalue'),'should be somevalue')
   end
 
   def test_source_current_data
-    FakeWeb.register_uri(:get, "http://127.0.0.1:9999/foo", :body => JSON::dump({:heres => "some json"}))
-    source = Source.new(:name => 'test', :href=> 'http://127.0.0.1:9999/foo',:frequency => 1)
+    FakeWeb.register_uri(:get, "http://127.0.0.1:9999/for", :body => JSON::dump({:heres => "some json"}))
+    source = Source.new(:name => 'test', :href=> 'http://127.0.0.1:9999/for',:frequency => 1)
     assert_nothing_raised do 
       assert_equal({'heres' => "some json"},source.current_data, "Something amiss: #{source.current_data.inspect}")
     end
   end
 
   def test_view_jobs
-    FakeWeb.register_uri(:get, "http://127.0.0.1:9999/foo", :body => JSON::dump({"jobs"=>[{"color"=>"red", "url"=>"http://localhost:8080/job/failjob/", "name"=>"failjob"}]}))
-    source1 = Source.new(:name => 'test', :href=> 'http://127.0.0.1:9999/foo', :frequency => 1)
-    source1.save
-    FakeWeb.register_uri(:get, "http://127.0.0.1:9998/bar", :body => JSON::dump({"jobs"=>[{"color"=>"blue", "url"=>"http://localhost:8080/job/goodjob/", "name"=>"goodjob"}]}))
-    source2 = Source.new(:name => 'test2', :href=> 'http://127.0.0.1:9998/bar',:frequency => 1)
-    source2.save
-    view = View.new(:title => 'A view')
-    view.sources << source1
-    view.sources << source2
-    view.save!
-    assert view.sources.length == 2
-    assert view.jobs.length == 2
+    assert @client.views.first.jobs.length == 2
   end
 
   def client_for_tests
+    FakeWeb.register_uri(:get, "http://127.0.0.1:9999/foo", :body => JSON::dump({"jobs"=>[{"color"=>"red", "url"=>"http://localhost:8080/job/failjob/", "name"=>"failjob"}]}))
+    FakeWeb.register_uri(:get, "http://127.0.0.1:9998/bar", :body => JSON::dump({"jobs"=>[{"color"=>"blue", "url"=>"http://localhost:8080/job/goodjob/", "name"=>"goodjob"}]}))
     client = Client.create(:location => 'test')
     client.ip = '127.0.0.1'
-    client.save
+    client.save!
+    source1 = Source.create(:name => 'test', :href=> 'http://127.0.0.1:9999/foo', :frequency => 1)
+    source2 = Source.create(:name => 'test2', :href=> 'http://127.0.0.1:9998/bar',:frequency => 1)
+    view = View.create(:title => 'A view')
+    view.sources << source1
+    view.sources << source2
+    view.save!
+    client.views << view
+    client.save!
     client
   end
 
